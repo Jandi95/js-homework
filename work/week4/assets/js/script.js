edea77f1369bf73902df5059d9e03e9aea875a43
Jandi95 <dduk2684@naver.com>
2025-07-05 18:04:19 +0900

    [feat] 4주차 과제 기능구현

--- work/week4/assets/js/script.js
+++ work/week4/assets/js/script.js
@@ -1,1 +1,30 @@
 /* global gsap */
+const txtAnim = gsap.timeline({
+  defaults: {duration: 1, opacity: 0},
+})
+.from('.txt-wrap h2', {y: '-200%'})
+.from('.txt-wrap strong', {y: '-100%'}, '-=90%')
+.from('.txt-wrap p', {y: '-100%'}, '-=90%')
+.from('.txt-wrap ul', {y: '-100%'}, '-=90%')
+
+const imgAnim = gsap.timeline({
+  repeat: -1,
+  repeatDelay: 0.6,
+  yoyo: true,
+  defaults: {duration: 0.6,},
+})
+.to('.shape01', {rotate: 360}, 1)
+.to('.shape04', {x: '-10%', ease: 'bounce.out'}, '-=70%')
+.to('.shape06', {transformOrigin: 'right bottom', rotate: 90, ease: 'bounce.out'}, '<')
+.to('.shape09', {transformOrigin: 'right top', rotate: -90, ease: 'bounce.out'}, '<')
+.to('.shape12', {y: '50%', ease: 'bounce.out'}, '<')
+.to('.shape03', {rotate: -90, transformOrigin: 'left bottom', ease: 'bounce.out'}, '-=50%')
+.to('.shape02', {x: '-60%'}, '<')
+.to('.shape11', {y: '-100%', ease: 'bounce.in'}, '<')
+.to('.shape08', {rotate: 180, ease: 'bounce.out'}, '-=80%')
+.to('.shape13', {scale: 0.7, opacity: 0.7, duration: 0.2}, '-=80%')
+.to('.shape13', {scale: 1, opacity: 1, duration: 0.2})
+.to('.shape15', {rotate: -360})
+
+
+// GSDevTools.create()
